e79c9068432eded36cb5686a096b43d723a5da24
Jiahui Xu <jxu@ethz.ch>
2022-12-17 14:03:33 +0100

    add small parser

--- examples/fir/main.sh
+++ examples/fir/main.sh
@@ -5,12 +5,9 @@
 PROJ_DIR="."
 PROJ_NAME=$(basename $(ls ${PROJ_DIR}/src/*.cpp| head -1) | sed 's/.cpp//g')
 TOP_FILE="${PROJ_NAME}.cpp"
-rm $PROJ_NAME* 2> /dev/null
+mkdir reports 2> /dev/null
 
 # generate SSA
-gcc -fdump-tree-ssa -S -O0 ./src/$TOP_FILE
-
-# generate CFG
-gcc -fdump-tree-cfg -S -O0 ./src/$TOP_FILE
+gcc -fno-inline -fdump-tree-ssa -fdump-tree-cfg -dumpdir ./reports/ -S -O3 ./src/$TOP_FILE
 
 } 2>&1 | tee output.log
