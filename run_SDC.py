b07456b0c14a7d8c9fd75299188c7b8307479c19
Your Name <crizzi@ethz.ch>
2023-01-10 12:56:46 +0100

    Updated log object and comments in the run_SDC main file

--- run_SDC.py
+++ run_SDC.py
@@ -4,29 +4,26 @@ from src.main_flow.parser import Parser
 from src.main_flow.scheduler import Scheduler
 import logging
 
+# create log interface
 log = logging.getLogger('sdc')
-log.setLevel(logging.DEBUG)
+log.setLevel(logging.INFO)
+console_handler = logging.StreamHandler() # create console handler and set level to debug
+formatter = logging.Formatter('%(levelname)s - %(message)s') # create formatter
+console_handler.setFormatter(formatter) # add formatter to console
+log.addHandler(console_handler) # add console_handler to log
 
-# create console handler and set level to debug
-ch = logging.StreamHandler()
-ch.setLevel(logging.DEBUG)
-
-# create formatter
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-
-# add formatter to ch
-ch.setFormatter(formatter)
-
-# add ch to log
-log.addHandler(ch)
 
 def main(args):
 	frontend_only = args.frontend
 	input_list = args.input_list
 	base_path = args.examples_folder
+	debug_mode = args.debug
 
-	log.info("Arguments selected:\n\tINPUT LIST = {0}\n\tEXAMPLE FOLDER PATH = {1}\n\tONLY FRONTEND = {2}".format(frontend_only, base_path, input_list))
-	print("[INFO] Arguments selected:\n\tINPUT LIST = {0}\n\tEXAMPLE FOLDER PATH = {1}\n\tONLY FRONTEND = {2}".format(frontend_only, base_path, input_list))
+	log.info("Arguments selected:\n\tINPUT LIST = {0}\n\tEXAMPLE FOLDER PATH = {1}\n\tONLY FRONTEND = {2}\n\tDEBUG = {3}".format(frontend_only, base_path, input_list, debug_mode))
+
+	# if debug_mode is selected logger is set to debug
+	if debug_mode:
+		log.setLevel(logging.DEBUG)
 
 	#reading the list of examples to execute
 	examples_list_file = open(input_list, "r")
@@ -39,12 +36,10 @@ def main(args):
 		# the path of the ssa file should be base_path/example_name/reports/example_name.cpp_mem2reg_constprop_simplifycfg_die.ll
 		path_ssa_example = "{0}/{1}/reports/{1}.cpp_mem2reg_constprop_simplifycfg_die.ll".format(base_path, example_name)
 
-		log.info("Parsing file {0}".format(path_ssa_example)) 
-		print("[Info] Parsing file {0}".format(path_ssa_example))
+		log.info("Parsing file {0}".format(path_ssa_example))
 		ssa_parser = Parser(path_ssa_example, example_name)
 		if not(ssa_parser.is_valid()):
 			log.error("Parser has encountered a problem. Please verify path correctness ({0})".format(path_ssa_example))
-			print("[ERROR] Parser has encountered a problem. Please verify path correctness ({0})".format(path_ssa_example))
 			continue
 		ssa_parser.draw_cdfg("{0}/{1}/test.pdf".format(base_path, example_name))
 
@@ -56,7 +51,7 @@ def main(args):
 		scheduler.find_optimal_schedule(base_path, example_name)
 
 	if frontend_only:
-		print("[Info] Early execution termination\n\nBye :)")
+		log.info("Early execution termination\n\nBye :)")
 
 
 if __name__ == '__main__':
@@ -64,6 +59,7 @@ if __name__ == '__main__':
 	arg_parser.add_argument('--input_list', type=str, help='Input filelist containing examples to run', default="filelist.lst")
 	arg_parser.add_argument('--examples_folder', type=str, help='Path of the examples folder', default="examples")
 	arg_parser.add_argument('--frontend', action='store_true' , help='Execute only frontend', default=False)
+	arg_parser.add_argument('--debug', action='store_true' , help='Set debug mode', default=False)
 
 
 	args = arg_parser.parse_args()
