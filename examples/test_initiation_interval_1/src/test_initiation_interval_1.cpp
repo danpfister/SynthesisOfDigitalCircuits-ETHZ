36d005a3ab6087535e192344058dbfa12bb8f719
Jiahui Xu <jxu@ethz.ch>
2023-01-24 22:54:20 +0100

    update benchmark: add test initiation interval 1

---
+++ examples/test_initiation_interval_1/src/test_initiation_interval_1.cpp
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+
+int test_initiation_interval_1 (int a[], int b[], int c[]) {
+	int acc = 0;
+	for (int i = 0; i < 100; ++i) {
+		acc += ( (a[i] % 2) * (b[i] % 3) + (b[i] % 3) );
+	}
+	return acc;
+}
+
+int main(void){
+	int a[100], b[100], c[100];
+	for (int i = 0; i < 100; ++i) {
+		a[i] = rand() % 10;
+		b[i] = rand() % 10;
+		c[i] = rand() % 10;
+	}
+	test_initiation_interval_1(a, b, c);
+}
+
