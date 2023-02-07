9821f894d6d41f71e5cf29ac2324eac151d65aee
Your Name <crizzi@ethz.ch>
2023-02-07 20:37:20 +0100

    Removed examples src codes, figures and others

--- run_generate_ir.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-pwd
-cat ./filelist.lst | while read filename; do
-	echo "Running LLVM frontend for example \"${filename}\""
-	( cd examples/${filename}; ls; ../../src/main_flow/llvm_ir.sh )
-	
-done
